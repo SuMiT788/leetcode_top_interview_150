3e7f17c44babb3a711f9ffe2b74188ba60b46597
Sumit Kale <sumitkale788@gmail.com>
2025-12-14 13:02:56 +0530

    mergeSortedArray

---
+++ Array/mergeSortedArray.js
@@ -0,0 +1,52 @@
+/**
+ * https://leetcode.com/problems/merge-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
+ *
+ * You are given two integer arrays nums1 and nums2, sorted in non-decreasing order,
+ * and two integers m and n, representing the number of elements in nums1 and nums2
+ * respectively.
+ *
+ * Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+ *
+ * The final sorted array should not be returned by the function,
+ * but instead be stored inside the array nums1. To accommodate this,
+ * nums1 has a length of m + n, where the first m elements denote the elements that should be
+ * merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+ *
+ * Topics: Array, Two Pointers, Sorting
+ */
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+  const temp = nums1.slice(0, m);
+
+  let i = (j = k = 0);
+  while (i < m && j < n) {
+    if (temp[i] < nums2[j]) {
+      nums1[k] = temp[i];
+      i++;
+    } else {
+      nums1[k] = nums2[j];
+      j++;
+    }
+    k++;
+  }
+
+  while (i < m) {
+    nums1[k] = temp[i];
+    i++;
+    k++;
+  }
+  while (j < n) {
+    nums1[k] = nums2[j];
+    j++;
+    k++;
+  }
+};
+
+merge([1, 2, 3, 4, 5], 5, [], 0);
